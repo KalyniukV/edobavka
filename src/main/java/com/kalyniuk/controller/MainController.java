4b18830f1c1717d60ad5ff875d32cbfcd96383ee
KalyniukV <kvit89@gmail.com>
2019-12-19 14:49:51 +0200

    Update MainController.java

--- src/main/java/com/kalyniuk/controller/MainController.java
+++ src/main/java/com/kalyniuk/controller/MainController.java
@@ -1,25 +1,30 @@
 package com.kalyniuk.controller;
 
-import com.viber.bot.api.ViberBot;
 
+import com.viber.bot.api.ViberBot;
 import org.springframework.stereotype.Controller;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.inject.Inject;
+
 @Controller
 public class MainController {
+
+    @Inject
+    private ViberBot bot;
 
     @GetMapping("/")
     public String main() {
         return "index";
     }
-    
+
     @PostMapping("/")
     public String sendMessage(@RequestParam String text) {
-        System.out.println("send message = " + text);
+        System.out.println("send text = " + text);
         bot.onTextMessage("(hi|hello)", (event, message, response) -> response.send(text + " " + event.getSender().getName()));
         return "index";
     }
-
 }
