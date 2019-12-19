9d91c80a53f3056b2f7eeb8c98a58c8f133087a7
KalyniukV <kvit89@gmail.com>
2019-12-19 17:23:06 +0200

    Update MainController.java

--- src/main/java/com/kalyniuk/controller/MainController.java
+++ src/main/java/com/kalyniuk/controller/MainController.java
@@ -1,24 +1,44 @@
 package com.kalyniuk.controller;
 
 
+import com.viber.bot.api.ViberBot;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.inject.Inject;
+
 
 @Controller
 public class MainController {
 
+    @Inject
+    private ViberBot bot;
+    
+    @Value("${application.viber-bot.webhook-url}")
+    private String webhookUrl;
+
     @GetMapping("/")
     public String main() {
+        System.out.println("=== MainController.main() ===");
         return "index";
     }
 
     @PostMapping("/")
-    public String sendMessage(@RequestParam String message) {
-        System.out.println("send message = " + message);        
+    public String sendMessage(@RequestParam String text) {
+        System.out.println("=== MainController.sendMessage text = " + text);
+        try {
+            bot.setWebhook(webhookUrl).get();
+
+            System.out.println("=== MainController accountInfo = " + bot.getAccountInfo().get());
+            
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+
         return "index";
     }
 }
