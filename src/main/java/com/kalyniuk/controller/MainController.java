82e739b83314aa5faf67208370dbe9c0b02d073f
KalyniukV <kvit89@gmail.com>
2019-12-20 10:27:18 +0200

    Update MainController.java

--- src/main/java/com/kalyniuk/controller/MainController.java
+++ src/main/java/com/kalyniuk/controller/MainController.java
@@ -24,18 +24,19 @@ public class MainController {
 
     @PostMapping("/")
     public String sendMessage(@RequestParam String message) {
-        System.out.println("send message = " + message);
+        System.out.println("=== send message = " + message);
         String jsonString = "{" +
-                "   \"receiver\":\"OGdRykRL2GmdB2xKg+NE5g==\"," +
-                "   \"min_api_version\":1," +
-                "   \"sender\":{\n" +
-                "      \"name\":\"edobavka\"," +
-                "      \"avatar\":\"https://images-na.ssl-images-amazon.com/images/I/51-aTeYbibL._SY355_.png\"" +
-                "   },\n" +
-                "   \"tracking_data\":\"tracking data\"," +
-                "   \"type\":\"text\"," +
-                "   \"text\":\"Hello world!\"" +
+                "\"receiver\":\"OGdRykRL2GmdB2xKg+NE5g==\"," +
+                "\"min_api_version\":1," +
+                "\"sender\":{\n" +
+                "\"name\":\"edobavka\"," +
+                "\"avatar\":\"https://images-na.ssl-images-amazon.com/images/I/51-aTeYbibL._SY355_.png\"" +
+                "}," +
+                "\"tracking_data\":\"tracking data\"," +
+                "\"type\":\"text\"," +
+                "\"text\":\"Hello world!\"" +
                 "}";
+        System.out.println("=== jsonString = " + jsonString);
         bot.incoming(Request.fromJsonString(jsonString));
         return "index";
     }
