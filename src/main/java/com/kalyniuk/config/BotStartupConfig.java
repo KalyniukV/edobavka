18d8627265728e3e78faa6bdad1dbcc0e0ec44e6
KalyniukV <kvit89@gmail.com>
2019-12-21 10:15:11 +0200

    Update BotStartupConfig.java

--- src/main/java/com/kalyniuk/config/BotStartupConfig.java
+++ src/main/java/com/kalyniuk/config/BotStartupConfig.java
@@ -1,69 +1,29 @@
 package com.kalyniuk.config;
 
-
-import com.google.common.base.Charsets;
-import com.google.common.base.Preconditions;
-import com.google.common.io.CharStreams;
 import com.kalyniuk.service.ViberBotService;
-import com.viber.bot.Request;
-import com.viber.bot.ViberSignatureValidator;
 import com.viber.bot.api.ViberBot;
-
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-
-import javax.annotation.Nullable;
 import javax.inject.Inject;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.concurrent.ExecutionException;
+
 
 
 @Configuration
 public class BotStartupConfig implements ApplicationListener<ApplicationReadyEvent> {
+
     @Inject
     private ViberBot bot;
-
-    @Inject
-    private ViberSignatureValidator signatureValidator;
-
-    @Value("${application.viber-bot.webhook-url}")
-    private String webhookUrl;
 
     @Autowired
     private ViberBotService viberService;
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
-        try {
-            System.out.println("=== BotStartupConfig onApplicationEvent before setWebhook ===");
-            bot.setWebhook(webhookUrl).get();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         bot.onMessageReceived((event, message, response) -> viberService.onMessageReceived(event, message, response));
         bot.onConversationStarted(event -> viberService.onConversationStarted(event));
-//        bot.onSubscribe((event, response) -> viberService.onSubscribe(event, response));
-//        bot.onUnsubscribe((event) -> viberService.onUnsubscribe(event));
-    }
-
-    @PostMapping(value = "/", produces = "application/json")
-    public String incoming(@RequestBody String json,
-                           @RequestHeader("X-Viber-Content-Signature") String serverSideSignature)
-            throws ExecutionException, InterruptedException, IOException {
-        System.out.println("=== BotStartupConfig incoming ===");
-        Preconditions.checkState(signatureValidator.isSignatureValid(serverSideSignature, json), "invalid signature");
-        @Nullable InputStream response = bot.incoming(Request.fromJsonString(json)).get();
-        return response != null ? CharStreams.toString(new InputStreamReader(response, Charsets.UTF_16)) : null;
     }
 
 }
