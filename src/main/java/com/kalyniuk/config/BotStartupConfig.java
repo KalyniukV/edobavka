8c3e0d697317ce3b62dd46b03c9347dc48a0d6b9
KalyniukV <kvit89@gmail.com>
2019-12-21 10:39:51 +0200

    Update BotStartupConfig.java

--- src/main/java/com/kalyniuk/config/BotStartupConfig.java
+++ src/main/java/com/kalyniuk/config/BotStartupConfig.java
@@ -4,6 +4,7 @@ import com.kalyniuk.service.ViberBotService;
 import com.viber.bot.api.ViberBot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Configuration;
@@ -13,15 +14,24 @@ import javax.inject.Inject;
 
 @Configuration
 public class BotStartupConfig implements ApplicationListener<ApplicationReadyEvent> {
-
     @Inject
     private ViberBot bot;
 
     @Autowired
     private ViberBotService viberService;
 
+    @Value("${application.viber-bot.webhook-url}")
+    private String webhookUrl;
+
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+        try {
+            System.out.println("=== BotStartupConfig onApplicationEvent before setWebhook ===");
+            bot.setWebhook(webhookUrl).get();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
         bot.onMessageReceived((event, message, response) -> viberService.onMessageReceived(event, message, response));
         bot.onConversationStarted(event -> viberService.onConversationStarted(event));
     }
