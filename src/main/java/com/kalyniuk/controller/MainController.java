b94b31da9ad93b4acb7d24b38a1bee0d8e50d4cf
KalyniukV <kvit89@gmail.com>
2019-12-21 10:16:05 +0200

    Update MainController.java

--- src/main/java/com/kalyniuk/controller/MainController.java
+++ src/main/java/com/kalyniuk/controller/MainController.java
@@ -1,27 +1,20 @@
 package com.kalyniuk.controller;
 
-import com.google.common.collect.ImmutableMap;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+
+import com.kalyniuk.service.ViberBotService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.Map;
 
 
 @Controller
 public class MainController {
 
-    private static final String URL = "https://chatapi.viber.com/pa/send_message";
-    private static final String VIBER_HEADER = "X-Viber-Auth-Token";
-
-    @Value("${application.viber-bot.auth-token}")
-    private  String authorizationToken;
+    @Autowired
+    ViberBotService service;
 
     @GetMapping("/")
     public String main() {
@@ -30,18 +23,7 @@ public class MainController {
 
     @PostMapping("/")
     public String sendMessage(@RequestParam String message) {
-        HttpHeaders headers = new HttpHeaders();
-        headers.set(VIBER_HEADER, authorizationToken);
-        String userId = "OGdRykRL2GmdB2xKg+NE5g==";
-        Map<String, Object> msg = ImmutableMap.of(
-                "receiver", userId,
-                "type", "text",
-                "text", message,
-                "sender", ImmutableMap.of("name", "edobavka")
-        );
-        HttpEntity<Map> request = new HttpEntity<>(msg, headers);
-
-        new RestTemplate().postForEntity(URL, request, Map.class);
+        service.sendMessage(message);
         return "index";
     }
 }
